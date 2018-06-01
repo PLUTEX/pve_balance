c78bc97cdfff22552d40680ea65298ac17d587ff
Jan-Philipp Litza <jpl@plutex.de>
2018-06-01 16:36:11 +0200

    algorithm: Loop over source/target host pairs

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,7 @@
+from itertools import product
 from operator import attrgetter
-from helper import get_logger
 
+from helper import get_logger
 from model import Migration
 
 logger = get_logger(__name__)
@@ -67,78 +68,92 @@ def calculate_migrations(hosts, exclude=[], threshold=1024**3):
             max_imbalance(hosts),
         )
 
-        # Migrate from the most over-staffed to the most under-staffed host
-        source_host = hosts[-1]
-        target_host = hosts[0]
-        assert target_host not in exclude
-        logger.debug(
-            'Trying migrating from host {} to host {}',
-            source_host.name,
-            target_host.name,
-        )
-
-        # In case of a tie (see below criterion), prefer VMs with little
-        # memory, as they migrate faster
-        vms = sorted(source_host.vms, key=attrgetter('used_memory'))
-
-        # Prefer migrating VMs that bring us closest to the target usage
-        vms.sort(key=lambda vm: abs(vm.used_memory + source_host.memory_imbalance))
-
-        for vm in vms:
-            if vm in (migration.vm for migration in migrations):
-                logger.debug(
-                    'VM {} is already planned for migration, ignoring it',
-                    vm.id,
-                )
+        # Migrate from the most over-loaded to the most under-loaded host
+        for source_host, target_host in product(reversed(hosts), hosts):
+            if source_host.memory_imbalance > 0:
                 continue
 
-            if vm.used_memory == 0:
+            if target_host.memory_imbalance < 0:
                 continue
 
-            if vm.used_memory > -source_host.memory_imbalance + threshold:
-                logger.debug(
-                    "VM {0.id} (memory={0.used_memory!b}) overshoots source "
-                    "host's imbalance of {1.memory_imbalance!b} by more "
-                    "than {2!b}",
-                    vm, source_host, threshold,
-                )
+            if target_host in exclude:
                 continue
 
-            if vm.used_memory > target_host.memory_imbalance + threshold:
-                logger.debug(
-                    "VM {0.id} (memory={0.used_memory!b}) overshoots target "
-                    "host's imbalance of {1.memory_imbalance!b} by more "
-                    "than {2!b}",
-                    vm, target_host, threshold,
-                )
-                if source_host in exclude:
-                    logger.info(
-                        "Migrating VM {0.id} from host {1.name} to host "
-                        "{2.name} despite overshooting memory imbalance by "
-                        "{3!b}, because we need to empty {1.name}",
-                        vm, source_host, target_host,
-                        vm.used_memory - target_host.memory_imbalance,
-                    )
-                else:
-                    continue
-
-            # This migration seems useful, remember it and update the memory
-            # imbalance of the hosts associated
-            logger.info(
-                "Planning migration of VM {} from host {} to host {}",
-                vm.id,
+            logger.debug(
+                'Trying migrating from host {} to host {}',
                 source_host.name,
                 target_host.name,
             )
-            migrations.append(Migration(vm, target_host))
-            source_host.memory_imbalance += vm.used_memory
-            target_host.memory_imbalance -= vm.used_memory
 
-            # Break the VM loop to re-order hosts first
+            # In case of a tie (see below criterion), prefer VMs with little
+            # memory, as they migrate faster
+            vms = sorted(source_host.vms, key=attrgetter('used_memory'))
+
+            # Prefer migrating VMs that bring us closest to the target usage
+            vms.sort(key=lambda vm: abs(vm.used_memory + source_host.memory_imbalance))
+
+            for vm in vms:
+                if vm in (migration.vm for migration in migrations):
+                    logger.debug(
+                        'VM {} is already planned for migration, ignoring it',
+                        vm.id,
+                    )
+                    continue
+
+                if vm.used_memory == 0:
+                    continue
+
+                if vm.used_memory > -source_host.memory_imbalance + threshold:
+                    logger.debug(
+                        "VM {0.id} (memory={0.used_memory!b}) overshoots source "
+                        "host's imbalance of {1.memory_imbalance!b} by more "
+                        "than {2!b}",
+                        vm, source_host, threshold,
+                    )
+                    continue
+
+                if vm.used_memory > target_host.memory_imbalance + threshold:
+                    logger.debug(
+                        "VM {0.id} (memory={0.used_memory!b}) overshoots target "
+                        "host's imbalance of {1.memory_imbalance!b} by more "
+                        "than {2!b}",
+                        vm, target_host, threshold,
+                    )
+                    if source_host in exclude:
+                        logger.info(
+                            "Migrating VM {0.id} from host {1.name} to host "
+                            "{2.name} despite overshooting memory imbalance by "
+                            "{3!b}, because we need to empty {1.name}",
+                            vm, source_host, target_host,
+                            vm.used_memory - target_host.memory_imbalance,
+                        )
+                    else:
+                        continue
+
+                # This migration seems useful, remember it and update the memory
+                # imbalance of the hosts associated
+                logger.info(
+                    "Planning migration of VM {} from host {} to host {}",
+                    vm.id,
+                    source_host.name,
+                    target_host.name,
+                )
+                migrations.append(Migration(vm, target_host))
+                source_host.memory_imbalance += vm.used_memory
+                target_host.memory_imbalance -= vm.used_memory
+
+                # Break the VM loop to re-order hosts first
+                break
+            else:
+                # No VMs to migrate between these hosts, try the next pair
+                continue
+
+            # VM loop breaked, so we migrated something. Break the host pair
+            # loop as well
             break
 
         else:
-            # If the VM loop terminated without breaking, we didn't plan any
+            # If the host loop terminated without breaking, we didn't plan any
             # migration from the most over-staffed to the most under-staffed
             # host. So apparently, we're done.
             break
